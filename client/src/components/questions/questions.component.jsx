c0a6154db31c4f43df1cd2030a5c4f20323de782
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-06 15:18:44 -0500

    add value for questions

--- client/src/components/questions/questions.component.jsx
+++ client/src/components/questions/questions.component.jsx
@@ -1,17 +1,22 @@
 const Questions = [
   {
+    value: "q1",
     text: "1. I have felt cheerful and in good spirits",
   },
   {
+    value: "q2",
     text: "2. I have felt calm and relaxed",
   },
   {
+    value: "q3",
     text: "3. I have felt active and vigorous",
   },
   {
+    value: "q4",
     text: "4. I wake up feeling fresh and rested",
   },
   {
+    value: "q5",
     text: "5. My daily life has been filled with things that interest me",
   },
 ];
