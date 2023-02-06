4b2c2a4716982342fd4821b2b48c4ae0ec15d958
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-06 15:21:39 -0500

    donePage component(no data)

---
+++ client/src/routes/donePage/donePage.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Model } from "survey-core";
+import { Survey } from "survey-react-ui";
+import "survey-core/defaultV2.min.css";
+import { json } from "../../components/surveyJson/json.component";
+function Done({ submittedData }) {
+  if (!submittedData) {
+    return <div>Please first finish the form</div>;
+  }
+
+  const { survey } = submittedData;
+
+  const newSurvey = new Model(json);
+  newSurvey.onComplete.add((sender, options) => {
+    console.log(JSON.stringify(sender.data, null, 3));
+  });
+  newSurvey.data = {
+    survey: {
+      q1: survey["q1"],
+      q2: survey["q2"],
+      q3: survey["q3"],
+      q4: survey["q4"],
+      q5: survey["q5"],
+    },
+  };
+  newSurvey.mode = "display";
+
+  return (
+    <div>
+      <h1 className="surveyName">Week 2 WHO-5 Well-Being Index</h1>
+      <Survey model={newSurvey} />
+    </div>
+  );
+}
+
+export default Done;
