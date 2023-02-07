664489d3cf36a596ed7701323cf9f04132dfee8b
Huili Zheng <hz2710@cumc.columbia.edu>
2023-02-06 22:13:37 -0500

    make timepoint as longitudinal index

--- server/routes/Survey.js
+++ server/routes/Survey.js
@@ -3,27 +3,24 @@ const router = express.Router();
 const { WhoForms } = require("../models");
 
 router.get("/", async (req, res) => {
-  //   res.render("forms/index");
   const answers = await WhoForms.findAll();
   res.json(answers);
 });
 
-// router.get("/survey/:id", async (req, res) => {
-//   const id = req.params.id;
-//   const form = await WhoForms.findByPk(id);
-//   if (!form) {
-//     return res.status(404).json({ error: "Form not found" });
-//   }
-//   res.json(form);
-//   console.log(form);
-// });
-
 router.post("/survey", async (req, res) => {
   const surveyData = req.body.survey;
-  surveyData.uid = 1;
+  surveyData.uid = 2;
   surveyData.status = 1;
-  surveyData.timepoint = 100;
-  surveyData.created_timestamp = 100;
+  // Query the database to get the latest submission count for this user
+  const latestSubmission = await WhoForms.findOne({
+    where: { uid: surveyData.uid },
+    order: [["created_timestamp", "DESC"]],
+  });
+
+  // Increment the submission count
+  surveyData.timepoint = latestSubmission ? latestSubmission.timepoint + 1 : 1;
+
+  surveyData.created_timestamp = Math.floor(Date.now() / 1000);
 
   await WhoForms.create(surveyData);
   console.log(surveyData);
